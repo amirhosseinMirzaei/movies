08d0dee57e8e73ed132e88a2fed553f1a29627e4
AmirHossein <amirhossein1mirzaei79@gmail.com>
2025-05-15 04:53:33 -0700

    first-commit

---
+++ movie-project/src/components/MovieCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+
+export const MovieCard = ({ movie }) => {
+  function onFavoriteClick() {
+    alert("click");
+  }
+
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={movie.url} alt={movie.title} />
+        <div className="movie-overlay">
+          <button className="favorite-btn" onClick={onFavoriteClick}>
+            <FontAwesomeIcon icon={faHeart} />
+          </button>
+        </div>
+      </div>
+      <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date}</p>
+      </div>
+    </div>
+  );
+};
---
+++ movie-project/src/pages/Home.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { MovieCard } from "../components/MovieCard";
+import { useState } from "react";
+export const Home = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const movies = [
+    {
+      id: 1,
+      title: "John Wick",
+      release_date: "2020",
+    },
+    {
+      id: 2,
+      title: "Terminator",
+      release_date: "2020",
+    },
+    {
+      id: 3,
+      title: "Fall",
+      release_date: "2020",
+    },
+  ];
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchQuery("");
+  };
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="search for movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie}></MovieCard>
+        ))}
+      </div>
+    </div>
+  );
+};
