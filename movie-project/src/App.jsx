fd6ede4dd2d763e11a86ebb478a00fc4967f1611
AmirHossein <amirhossein1mirzaei79@gmail.com>
2025-05-17 01:50:54 -0700

    movieContext

--- movie-project/src/App.jsx
+++ movie-project/src/App.jsx
@@ -3,17 +3,20 @@ import Favorites from "./pages/Favorites";
 import { Home } from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import { NavBar } from "./components/NavBar";
+import { MovieProvider } from "./context/MovieContext";
 function App() {
   return (
-    <div>
-      <NavBar></NavBar>
-      <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
-          <Route path="/favorites" element={<Favorites />}></Route>
-        </Routes>
-      </main>
-    </div>
+    <MovieProvider>
+      <div>
+        <NavBar></NavBar>
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home></Home>}></Route>
+            <Route path="/favorites" element={<Favorites />}></Route>
+          </Routes>
+        </main>
+      </div>
+    </MovieProvider>
   );
 }
 
