3c4a8000faef33c070c622bddf13cd576ea46607
AmirHossein <amirhossein1mirzaei79@gmail.com>
2025-05-18 00:47:20 -0700

    favorites

--- movie-project/src/pages/Favorites.jsx
+++ movie-project/src/pages/Favorites.jsx
@@ -16,13 +16,13 @@ const Favorites = () => {
         </div>
       </div>
     );
-  }
-  return (
-    <div className="favorites-empty">
-      <h2>No Favorite movies yet</h2>
-      <p>Start adding movies to your favorites and they will appera here</p>
-    </div>
-  );
+  } else
+    return (
+      <div className="favorites-empty">
+        <h2>No Favorite movies yet</h2>
+        <p>Start adding movies to your favorites and they will appera here</p>
+      </div>
+    );
 };
 
 export default Favorites;
